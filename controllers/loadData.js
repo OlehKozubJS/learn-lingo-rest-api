424c6e05bc83fcaf21567842f5b28969b0df850e
OlehKozubJS <oleg.kozub54@gmail.com>
2024-02-06 00:52:42 +0200

    Update loadData.js

--- controllers/loadData.js
+++ controllers/loadData.js
@@ -3,7 +3,15 @@ const { loadFromFile } = require("../hooks");
 const loadData = async (request, response) => {
   const teachersString = await loadFromFile("teachers");
   const teachers = JSON.parse(teachersString);
-  await response.json(teachers);
+  const filteredTeachers = teachers.filter(
+    (teacher) =>
+      (language === "any language" || teacher.languages.includes(language)) &&
+      (level === "any level" || teacher.levels.includes(level)) &&
+      (price === "any price" ||
+        (Number(teacher.price_per_hour) >= Number(price.split(" ")[1]) &&
+          Number(teacher.price_per_hour) < Number(price.split(" ")[3])))
+  );
+  await response.json(filteredTeachers);
 };
 
 module.exports = { loadData };
